84d7e8d0627db019beadfd7c4802c84f9b1a0c68
Charles Clarke <Charles_B_Clarke@outlook.com>
2026-09-02 12:03:18 -0700

    Fixed the divide by zero error.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
-int main()
-{
-    std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-    std::cout << "Hi, please enter two whole numbers: ";
+int main() {
+  std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+  std::cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
-
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " << x * y << std::endl;
+  int x, y;
+  std::cin >> x >> y;
+  std::cout << "Addition: " << x + y << std::endl;
+  std::cout << "Subtraction: " << x - y << std::endl;
+  std::cout << "Multiplication: " << x * y << std::endl;
+  if (0 == y) {
+    std::cout << "Y is zero skipping division";
+  } else {
     std::cout << "Division: " << x / y << std::endl;
     std::cout << "Remainder: " << x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+  }
+  std::cout << "Square Root: " << sqrt(x) << std::endl;
+  std::cout << "Square: " << pow(x, y) << std::endl;
 
-    return 0;
+  return 0;
 }
